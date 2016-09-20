8182eba5d33f5b3a255f81083ac1061e916fa3a0
Traci Williams <traciwilliams@Tracis-MacBook-Pro.local>
2016-09-20 11:30:20 -0500

    window.onload. added

---
+++ scripts/app.js
@@ -0,0 +1,6 @@
+window.onload = function(even) {
+    //widow object's ability to do things onload
+    // use window.onload - to make sure your content is all loaded
+    console.log("The page has loaded!"); //the console will output something
+    console.log("Thanks, window.onload");
+};
